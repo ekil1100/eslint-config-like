b7679a5dc0d799519b9cbd3db709bd80f85655f4
lastingman <keithleaves@gmail.com>
2020-07-30 12:42:26 +0800

    feat: like like/typescript

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['standard', 'plugin:prettier/recommended', 'prettier/standard'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+}
---
+++ typescript.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
