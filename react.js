4102ffe733bc3707b322fedb8a3186e5e5eb2dfb
lastingman <keithleaves@gmail.com>
2020-07-30 13:01:43 +0800

    feat: react

---
+++ react.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier/react',
+  ],
+  rules: {},
+}
